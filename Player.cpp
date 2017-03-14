de73766a8cb8016a7c3d172955ddabf7d239138e
alexianebrao <alexianebrao@wifi-roaming-128-4-82-86.host.udel.edu>
2017-03-13 20:57:57 -0400

    char

--- Player.cpp
+++ Player.cpp
@@ -7,15 +7,16 @@ Player::Player() {
 // constructor for an actual human, that uses cin to ask the user for their name and the character they want to
 //use in the game, and initializes the name and c fields to the entered information.  It also initializes score to 0, and 
 //isComputer to false.
-	string i;
 	cout << "Please enter your name: ";
-	cin >> i;
+	cin >> name;
 	cout << "Which character do you want to be: ";
-	cin >> i;
+	cin >> character;
 
 }
 
 Player::Player(string n, char ic, bool x) {
-// constructor that takes as input parameters a name string, a character ic, and a Boolean value x and sets the name, the c, and the isComputer fields accordingly.  It initializes the score to 0
+// constructor that takes as input parameters a name string, a character ic,
+// and a Boolean value x and sets the name, the c, and the isComputer fields accordingly.  It initializes the score to 0
+
 
 }
